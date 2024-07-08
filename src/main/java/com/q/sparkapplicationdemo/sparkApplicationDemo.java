73fac04e3b6f3c02fefbeac9fb2dde012b8eaf16
q <kyug.shim@gmail.com>
2024-07-08 15:20:00 +0900

    [add]spark-session
    spark session 연결

--- src/main/java/com/q/sparkapplicationdemo/sparkApplicationDemo.java
+++ src/main/java/com/q/sparkapplicationdemo/sparkApplicationDemo.java
@@ -4,15 +4,17 @@ import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.SparkSession;
 
 
-public final class sparkApplicationDemo {
+public class sparkApplicationDemo {
     public static void main(String[] args) {
         SparkSession spark = SparkSession
                 .builder()
-                .appName("JavaSparkPi")
+                .appName("sparkApplicationDemo")
                 .master("local[*]")
+                .enableHiveSupport()
                 .getOrCreate();
 
-        System.out.println();
+        System.out.println("spark application started" + spark);
+        System.out.println("spark application started" + spark.version());
 
         spark.stop();
     }
