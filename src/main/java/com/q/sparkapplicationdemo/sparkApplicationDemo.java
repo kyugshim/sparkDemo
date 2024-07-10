1d2fb83589d7d2198008735c3af7c82ab54c66b1
q <kyug.shim@gmail.com>
2024-07-10 19:31:16 +0900

    [add]recover_구현

--- src/main/java/com/q/sparkapplicationdemo/sparkApplicationDemo.java
+++ src/main/java/com/q/sparkapplicationdemo/sparkApplicationDemo.java
@@ -1,18 +1,17 @@
 package com.q.sparkapplicationdemo;
 
-import org.apache.hadoop.fs.FSDataInputStream;
-import org.apache.hadoop.fs.FSDataOutputStream;
-import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.*;
 import org.apache.spark.sql.*;
 import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.Metadata;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
-import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.conf.Configuration;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class sparkApplicationDemo {
@@ -20,7 +19,7 @@ public class sparkApplicationDemo {
      * @param args
      * spark session - enable Hive
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SparkSession spark = SparkSession
                 .builder()
                 .appName("sparkApplicationDemo")
@@ -35,18 +34,24 @@ public class sparkApplicationDemo {
         spark.sql("SHOW DATABASES").show();
 
 
-        String csvDirectoryPath = "/Users/q_dev/Downloads";
-        String checkPointPath = "/Users/q_dev/project/sparkDemo/checkpoints";
-        String outputPath = "/Users/q_dev/Downloads/sparkDemo/parquet";
+        String csvDirectoryPath = "PATH/Downloads/2019-Oct.csv";
+        String checkPointPath = "PATH/project/sparkDemo/checkpoints";
+        String outputPath = "PATH/project/sparkDemo/parquet";
 
         dataProcessWithRecovery(spark, csvDirectoryPath, checkPointPath, outputPath);
 
         spark.stop();
     }
+
     /**
-     * CSV data 확인
+     *
+     * @param spark
+     * @param csvDirectoryPath
+     * @param checkPointPath
+     * @param outputPath
+     * @throws IOException
      */
-    public static void dataProcessWithRecovery(SparkSession spark, String csvDirectoryPath, String checkPointPath, String outputPath) {
+    public static void dataProcessWithRecovery(SparkSession spark, String csvDirectoryPath, String checkPointPath, String outputPath) throws IOException {
         try {
             // CSV 파일 data schema
             StructType schema = new StructType(new StructField[]{
@@ -66,8 +71,7 @@ public class sparkApplicationDemo {
             Dataset<Row> df = spark.read()
                     .option("header", "true")
                     .schema(schema)
-                    .csv(csvDirectoryPath + "/2019-Oct.csv")
-                    .limit(1);
+                    .csv(getNewFiles(csvDirectoryPath + "/last_batch", processedFiles));
 
             // KST 기준으로 daily 추가
             df = df.withColumn("event_date", functions.from_utc_timestamp(df.col("event_time"), "Asia/Seoul").cast("date"));
@@ -102,6 +106,7 @@ public class sparkApplicationDemo {
         } catch (Exception e) {
             // Log the error and handle recovery
             System.err.println("Batch processing failed: " + e.getMessage());
+            recoveryFromLastBatch(spark, checkPointPath, csvDirectoryPath, outputPath);
         }
     }
 
@@ -128,6 +133,57 @@ public class sparkApplicationDemo {
         out.close();
         System.out.println("Checkpoint successful");
     }
+
+    /**
+     *
+     * @param spark
+     * @param checkPointPath
+     * @param csvDirectoryPath
+     * @param outputPath
+     * @throws IOException
+     */
+    public static void recoveryFromLastBatch(SparkSession spark, String checkPointPath, String csvDirectoryPath, String outputPath) throws IOException {
+        try {
+            // hadoop FS를 사용해서 로컬 파일 사용
+            Configuration conf = new Configuration();
+            conf.set("fs.hdfs.impl", "file:///");
+            FileSystem fs = FileSystem.get(conf);
+            Path checkpointFile = new Path(checkPointPath + "/last_batch");
+            if (fs.exists(checkpointFile)) {
+                System.out.println("Checkpoint successful");
+
+                Set<String> processedFiles = getProcessedFiles(checkPointPath);
+                Dataset<Row> df = spark.read()
+                        .option("header", "true")
+                        .csv(getNewFiles(csvDirectoryPath, processedFiles));
+
+                if(df.isEmpty()) {
+                    System.out.println("no files found to process");
+                    return;
+                }
+
+                df = df.withColumn("event_date", functions.from_utc_timestamp(df.col("event_time"), "Asia/Seoul").cast("date"));
+
+                df.write()
+                        .partitionBy("event_date")
+                        .mode(SaveMode.Append)
+                        .parquet(outputPath);
+
+                checkPointSuccessfulBatch(checkPointPath, df.inputFiles());
+            } else {
+                System.out.println("no checkpoint found to process");
+            }
+        } catch (IOException e) {
+            System.out.println("recoveryFromLastBatch failed: " + e.getMessage());
+        }
+    }
+
+    /**
+     *
+     * @param checkPointPath
+     * @return
+     * @throws IOException
+     */
     private static Set<String> getProcessedFiles(String checkPointPath) throws IOException {
         // hadoop FS를 사용해서 로컬 파일 사용
         Configuration conf = new Configuration();
@@ -145,5 +201,31 @@ public class sparkApplicationDemo {
             in.close();
         }
         return processedFiles;
+    }
+
+    /**
+     *
+     * @param csvDirectoryPath
+     * @param processedFiles
+     * @return
+     * @throws IOException
+     */
+    private static String[] getNewFiles(String csvDirectoryPath, Set<String> processedFiles) throws IOException {
+        // hadoop FS를 사용해서 로컬 파일 사용
+        Configuration conf = new Configuration();
+        conf.set("fs.hdfs.impl", "file:///");
+        FileSystem fs = FileSystem.get(conf);
+        Path csvDirectory = new Path(csvDirectoryPath);
+
+        FileStatus[] fileStatuses = fs.listStatus(csvDirectory);
+
+        List<String> newFiles = new ArrayList<>();
+        for (FileStatus fileStatus : fileStatuses) {
+            String filePath = fileStatus.getPath().toString();
+            if (!processedFiles.contains(filePath)) {
+                newFiles.add(filePath);
+            }
+        }
+        return newFiles.toArray(new String[0]);
     }
 }
